e9ef76c5b0734c89f355547390876f7bb6c7dbba
ml-brent <brent.mc@medialand.tw>
2021-05-10 17:57:22 +0800

    lv1 開始

--- src/js/lv1.js
+++ src/js/lv1.js
@@ -1,2 +1,11 @@
 import '../assets/sass/style.scss'
+import * as THREE from 'three';
+import { render } from 'node-sass';
+import { PerspectiveCamera, WebGLRenderer } from 'three';
 
+const scene     = new THREE.Scene();
+const camera    = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer  = new WebGLRenderer({ antialias: true });
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
